085ac53c2a75cdca8c81daddc56055486bcf9b93
Nelcino Júnior <nelcino.junior@geoinova.com.br>
2016-04-16 09:09:27 -0300

    Ajustes

--- public/modules/condominio/prospectar/page.js
+++ public/modules/condominio/prospectar/page.js
@@ -1,23 +1,29 @@
 yum.define([
-	PI.Url.create('Condominio', '/prospectar/page.html'),
-	PI.Url.create('Condominio', '/prospectar/page.css')
-], function (html) {
+    PI.Url.create('Condominio', '/prospectar/page.html'),
+    PI.Url.create('Condominio', '/prospectar/page.css')
+], function(html) {
 
     Class('Condominio.Prospectar.Page').Extend(PI.Page).Body({
 
-        instances: function () {
+        instances: function() {
             this.view = new Mvc.View(html);
-            
+
             this.title = 'Pesquisa Prospecto';
+
+            this.observacao = new UI.RichText({
+                dataModel: 'Observacao',
+                placeholder: 'Informe uma observação',
+                autosize: true
+            });
         },
 
-        viewDidLoad: function () {
-            
+        viewDidLoad: function() {
+
         },
 
         events: {
-		
-		}
+
+        }
 
 
     });
